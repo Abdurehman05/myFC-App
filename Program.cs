e8c1da961add6648306a47731cfc08c820dc74fe
Abdu <abdukr5@gmail.com.com>
2020-11-12 21:42:02 -0800

    first c# commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace myFC_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nPlease enter your name");
+            var name  = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"\nHello , {name} on {date:d} and {date:t}!");
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
